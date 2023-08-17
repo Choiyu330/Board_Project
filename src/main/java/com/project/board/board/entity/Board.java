bd41f0a6ab689032eb73db05bca02e2d3d00714c
Choiyu330 <jenny4063@naver.com>
2023-08-18 02:14:28 +0900

    refactor: Board Entity 수정

--- src/main/java/com/project/board/board/entity/Board.java
+++ src/main/java/com/project/board/board/entity/Board.java
@@ -1,6 +1,7 @@
 package com.project.board.board.entity;
 
 import com.project.board.audit.Auditable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Data
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 public class Board extends Auditable {
 
     @Id

0ccce6595a06494ba8a3a9900cf419b3f3339f68
Choiyu330 <jenny4063@naver.com>
2023-08-19 00:31:14 +0900

    feat: User Entity 구현

---
+++ src/main/java/com/project/board/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.project.board.user.entity;
+
+import com.project.board.audit.Auditable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "USERS")
+public class User extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column
+    private Boolean isAdmin = false;
+}
